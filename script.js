8540cc6ded76a8b69444200f1c242b1f55110354
zaebbb <65035479+zaebbb@users.noreply.github.com>
2020-11-21 11:05:42 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+let nav = document.querySelector("#nav");
+let close = document.querySelector(".fa-times");
+
+function toggleBars(){
+    nav.classList.toggle("active");
+    close.classList.toggle("active");
+}
